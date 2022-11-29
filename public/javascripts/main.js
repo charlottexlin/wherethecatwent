8b87b93676ecad9eb9b4f39a26eeca3a147c4022
cxl229 <cxl229@nyu.edu>
2022-11-28 22:21:55 -0500

    changes to css class names

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,19 +1,19 @@
 // add functionality to web page
 function main() {
-    const bgs = document.querySelectorAll('.background');
-    for (const bg of bgs) {
-        bg.addEventListener('mouseenter', showCat);
-        bg.addEventListener('mouseleave', hideCat);
+    const scenes = document.querySelectorAll('.scene');
+    for (const scene of scenes) {
+        scene.addEventListener('mouseenter', showCat);
+        scene.addEventListener('mouseleave', hideCat);
     }
 }
 
-// function to call on mouse over a background - shows this background's cat
+// function to call on mouse over a scene - shows this scene's cat
 function showCat(evt) {
     const cat = evt.target.querySelector('.cat');
     cat.classList.remove('invisible');
 }
 
-// function to call on mouse leaving a background - hide's this background's cat
+// function to call on mouse leaving a scene - hide's this scene's cat
 function hideCat(evt) {
     const cat = evt.target.querySelector('.cat');
     cat.classList.add('invisible');
